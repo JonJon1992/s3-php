3664b50a9a54055a966aa0413adf5908e6234df1
Jonathan Neves <jonathan201392127@gmail.com>
2023-03-18 00:26:13 -0300

    fix: adicioando retorno do tipo Bucket

--- lib/helper.php
+++ lib/helper.php
@@ -1,9 +1,10 @@
 <?php
 
+use S3\Lib\Bucket;
 use S3\Lib\S3;
 
 if (!function_exists('s3')) {
-    function s3($alias = null)
+    function s3($alias = null):Bucket
     {
         return S3::singleton()->bucket($alias);
     }

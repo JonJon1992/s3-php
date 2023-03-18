da043037ebc16d6fb4cd0a7c0a20287dc788d25d
Jonathan Neves <jonathan201392127@gmail.com>
2023-03-18 00:40:38 -0300

    ajuste no composer.json

--- lib/helper.php
+++ lib/helper.php
@@ -4,7 +4,7 @@ use S3\Lib\Bucket;
 use S3\Lib\S3;
 
 if (!function_exists('s3')) {
-    function s3($alias = null):Bucket
+    function s3($alias = null): Bucket
     {
         return S3::singleton()->bucket($alias);
     }
